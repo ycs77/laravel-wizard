a97322990952216a7425e4db8ce1552c0ae89e93
Lucas Yang <yangchenshin77@gmail.com>
2019-10-25 15:29:44 +0800

    Fix step validation no custom message and attributes (#15)

--- src/Http/Controllers/WizardController.php
+++ src/Http/Controllers/WizardController.php
@@ -149,8 +149,14 @@ class WizardController extends Controller
 
         $step = $this->getWizardStep($request, $step);
 
+        // Form validation.
         if ($this->canValidate($request)) {
-            $this->validate($request, $step->rules($request));
+            $this->validate(
+                $request,
+                $step->rules($request),
+                $step->validateMessages($request),
+                $step->validateAttributes($request)
+            );
         }
 
         // Wizard step validated event.
