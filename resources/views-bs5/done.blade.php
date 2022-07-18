64780803394de7a4adc2c8f40a6c3efa8f9b0b00
Lucas Yang <yangchenshin77@gmail.com>
2022-07-18 15:45:21 +0800

    Change to @lang() in blade view

--- resources/views-bs5/done.blade.php
+++ resources/views-bs5/done.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container my-3">
         <h1 class="text-center">
-            {{ __($doneText ?? 'wizard::generic.done') }}
+            @lang($doneText ?? 'wizard::generic.done')
         </h1>
     </div>
 @endsection
